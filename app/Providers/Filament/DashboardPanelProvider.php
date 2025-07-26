8b7408696a1b0483ffe56a8233394e19ad64f1f5
Ozz-Matheus <montesinos.quintana@gmail.com>
2025-07-26 16:14:54 -0600

    DashboardPanelProvider

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -2,15 +2,22 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Dashboard\Resources\ActionResource\Widgets\ActionStatsOverview;
+use App\Filament\Dashboard\Resources\ActionResource\Widgets\ActionStatusChart;
+use App\Filament\Dashboard\Resources\ActionTaskResource\Widgets\UserTaskList;
+use App\Filament\Dashboard\Resources\DocResource\Widgets\DocStatsOverview;
+use App\Filament\Dashboard\Resources\DocResource\Widgets\DocStatusesChart;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\UserMenuItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -24,13 +31,28 @@ class DashboardPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
+        FilamentColor::register([
+            'primary' => Color::hex(config('filament-colors.primary.hex')),
+            'secondary' => Color::hex(config('filament-colors.secondary.hex')),
+            'indigo' => Color::hex(config('filament-colors.indigo.hex')),
+            'success' => Color::hex(config('filament-colors.success.hex')),
+            'danger' => Color::hex(config('filament-colors.danger.hex')),
+            'warning' => Color::hex(config('filament-colors.warning.hex')),
+            'darkextra' => Color::hex(config('filament-colors.darkextra.hex')),
+
+        ]);
+
         return $panel
             ->id('dashboard')
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::hex(config('filament-colors.primary.hex')),
             ])
+            ->favicon(asset('images/favicon.png'))
+            ->brandLogo(asset('images/record-manager-logo.svg'))
+            ->darkModeBrandLogo(asset('images/record-manager-logo-dark.svg'))
+            ->brandLogoHeight('3rem')
             ->discoverResources(in: app_path('Filament/Dashboard/Resources'), for: 'App\\Filament\\Dashboard\\Resources')
             ->discoverPages(in: app_path('Filament/Dashboard/Pages'), for: 'App\\Filament\\Dashboard\\Pages')
             ->pages([
@@ -38,8 +60,12 @@ class DashboardPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Dashboard/Widgets'), for: 'App\\Filament\\Dashboard\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                DocStatsOverview::class,
+                DocStatusesChart::class,
+                ActionStatsOverview::class,
+                ActionStatusChart::class,
+                UserTaskList::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -58,6 +84,34 @@ class DashboardPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])->databaseNotifications()
+            ->userMenuItems([
+                // Subproceso
+                UserMenuItem::make()
+                    ->label(function () {
+                        return auth()->user()?->leaderOfSubProcess()?->title;
+                    })
+                    ->icon('heroicon-o-puzzle-piece') // ícono para subproceso
+                    ->url(null)
+                    ->sort(2)
+                    ->hidden(fn () => ! auth()->user()?->leaderOfSubProcess() ||
+                        auth()->user()?->hasRole('super_admin') ||
+                        auth()->user()?->hasRole('admin')
+                    ),
+                // Proceso
+                UserMenuItem::make()
+                    ->label(function () {
+                        $subProcess = auth()->user()?->leaderOfSubProcess();
+
+                        return $subProcess?->process?->title;
+                    })
+                    ->icon('heroicon-o-rectangle-group') // ícono para proceso
+                    ->url(null)
+                    ->sort(1)
+                    ->hidden(fn () => ! auth()->user()?->leaderOfSubProcess() ||
+                        auth()->user()?->hasRole('super_admin') ||
+                        auth()->user()?->hasRole('admin')
+                    ),
             ]);
     }
 }
