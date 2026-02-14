51feed844b0a01ab0f5f1a257f6e89ab1aa8080d
Ozz-Matheus <montesinos.quintana@gmail.com>
2026-02-14 17:58:41 -0600

    Sábado Brutal

--- app/Notifications/DocCreatedNotice.php
+++ app/Notifications/DocCreatedNotice.php
@@ -56,7 +56,7 @@ class DocCreatedNotice extends Notification
     {
         return FilamentNotification::make()
             ->title($this->doc->title)
-            ->body(__('New document created'))
+            ->body(__('created_notice'))
             ->icon('heroicon-o-document-text')
             ->color('primary')
             ->status('primary')
