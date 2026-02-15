75e4380765eea438709b96a3974ae49d07f6ce73
Ozz-Matheus <montesinos.quintana@gmail.com>
2026-02-14 18:00:12 -0600

    Fix Sábado brutal

--- app/Notifications/DocCreatedNotice.php
+++ app/Notifications/DocCreatedNotice.php
@@ -56,7 +56,7 @@ class DocCreatedNotice extends Notification
     {
         return FilamentNotification::make()
             ->title($this->doc->title)
-            ->body(__('created_notice'))
+            ->body(__('doc.versions.created_notice'))
             ->icon('heroicon-o-document-text')
             ->color('primary')
             ->status('primary')
